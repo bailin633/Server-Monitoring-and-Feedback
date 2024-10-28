189ac67fb9c280e098134121d63873afaca44022
bailin633 <2412433138@qq.com>
2024-10-28 19:25:31 +0800

    update24/10/28

--- send_email.py
+++ send_email.py
@@ -2,6 +2,7 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import time
 
 # 邮箱配置映射
 EMAIL_SERVERS = {
@@ -14,8 +15,17 @@ EMAIL_SERVERS = {
     "sina.com": {"smtp_server": "smtp.sina.com", "smtp_port": 465},
 }
 
+# 上次发送的时间
+last_sent_time = 0  # 全局变量,记录上一次邮件发送的时间
+
 
 def send_alert_email(body, subject, to_email, email, password):
+    global last_sent_time
+    # 检测是否在5分钟内
+    current_time = time.time()
+    if current_time - last_sent_time < 30:  # 300/60 =5/min
+        print("五分钟内只能发送一封邮件")
+        return
     # 获取邮箱后缀，确定服务器
     domain = email.split("@")[-1]
     server_config = EMAIL_SERVERS.get(domain)
@@ -26,7 +36,7 @@ def send_alert_email(body, subject, to_email, email, password):
 
     smtp_server = server_config["smtp_server"]
     smtp_port = server_config["smtp_port"]
-
+    
     # 创建邮件
     msg = MIMEMultipart()
     msg['From'] = email
@@ -42,6 +52,8 @@ def send_alert_email(body, subject, to_email, email, password):
         server.login(email, password)
         server.sendmail(email, to_email, msg.as_string())
         server.quit()
+        # 更新上次发送的时间
+        last_sent_time = current_time
         print("Alert email sent successfully!")
     except Exception as e:
         print(f"Failed to send email: {e}")
