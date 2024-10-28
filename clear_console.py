7543287a1c07f52da7aded8db3f8b088f24afcb9
bailin633 <2412433138@qq.com>
2024-10-28 15:49:04 +0800

    update_x

--- clear_console.py
+++ clear_console.py
@@ -1,3 +1,4 @@
+
 import os
 
 
@@ -8,6 +9,3 @@ def clear_console():
         os.system('clear')  # Linux/Mac 系统
 
 
-def get_user_clear_time():
-    user_clear_time = int(input("请输入间隔多久清空控制台(秒):"))
-    return user_clear_time
---
+++ test.py
@@ -0,0 +1,4 @@
+from windows_info import get_cpu_core_count
+cpu_cores, cpu_cores_more = get_cpu_core_count()
+print(cpu_cores)
+print(cpu_cores_more)
